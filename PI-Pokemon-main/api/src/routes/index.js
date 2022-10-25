346cda498afe00048ca151afcdca84b48e283eb3
jonasojeda <jonojed98@gmail.com>
2022-10-25 14:45:29 -0300

    Creation pokemon Routes

--- PI-Pokemon-main/api/src/routes/index.js
+++ PI-Pokemon-main/api/src/routes/index.js
@@ -1,11 +1,15 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const typesRoute = require('./type.js');         //importando las rutas
+const pokemonsRoute = require('./pokemon.js');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.use("/pokemon", pokemonsRoute);  
+router.use("/type", typesRoute);    
 
 module.exports = router;
