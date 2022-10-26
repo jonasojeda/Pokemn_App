7d8c8194f35aa510b386192fa0ba3ba080522730
jonasojeda <jonojed98@gmail.com>
2022-10-26 15:24:02 -0300

    Preconnection to store

--- PI-Pokemon-main/client/src/App.js
+++ PI-Pokemon-main/client/src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import {Route, Switch, Redirect} from 'react-router'
 import './App.css';
+import LandingPage from './components/landingPage/LandingPage.jsx';
+import Home from './components/home/Home.jsx';
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Pokemon</h1>
+
+      <Switch>
+        <Route exact path={"/"} component={LandingPage} />
+        <Route path={"/home"} component={Home} />
+      </Switch>
+
     </div>
   );
 }
