b3fd0b34fc841b1725e6cf08b0d7a74b4891cc96
jonasojeda <jonojed98@gmail.com>
2022-11-05 20:15:41 -0300

    Last styles form component

--- PI-Pokemon-main/client/src/components/form/Form.jsx
+++ PI-Pokemon-main/client/src/components/form/Form.jsx
@@ -60,82 +60,87 @@ export default function Form() {
 
                 <div className="pokemonData">
                 <form onSubmit={event => handleSubmit(event)} className='divCreate'>
-                    <div className='divInputCreate'>
-                        <input
-                            type='text' name='name' onChange={handleInputChange} placeholder="Name" value={input.name} required autoComplete='off' className='inputCreate' />
-                    </div>
-                        {errors.name && (
-                            <p className='error-input'>{errors.name}</p>
-                        )}
-
-                    <div className='divInputCreate'>
-                        <input
-                            type='number' name='life' onChange={handleInputChange} value={input.life} placeholder='Life' min="0" max="255" required className='inputCreate' />
-                    </div>
-                        {errors.life && (
-                            <p className='error-input'>{errors.life}</p>
-                        )}
-
-                    <div className='divInputCreate'>
-                        <input
-                            type='number' name='attack' onChange={handleInputChange} value={input.attack} placeholder='Attack' min="0" max="255" required className='inputCreate' />
-                    </div>
-                        {errors.attack && (
-                            <p className='error-input'>{errors.attack}</p>
-                        )}
-
-                    <div className='divInputCreate'>
-                        <input
-                            type='number' name='defense' onChange={handleInputChange} value={input.defense} placeholder='Defense' min="0" max="255" required className='inputCreate' />
-                    </div>
-                        {errors.defense && (
-                            <p className='error-input'>{errors.defense}</p>
-                        )}
-
-                    <div className='divInputCreate'>
-                        <input
-                            type='number' name='speed' onChange={handleInputChange} value={input.speed} placeholder='Speed' min="0" max="255" required className='inputCreate' />
-                    </div>
-                    {errors.speed && (
-                        <p className='error-input'>{errors.speed}</p>
-                    )}
-
-                    <div className='divInputCreate'>
-                        <input
-                            type='number' name='height' onChange={handleInputChange} value={input.height} placeholder='Height' min="0" max="255" required className='inputCreate' />
-                    </div>
-                    {errors.height && (
-                        <p className='error-input'>{errors.height}</p>
-                    )}
-
-                    <div className='divInputCreate'>
-                        <input
-                            type='number' name='weight' onChange={handleInputChange} value={input.weight} placeholder='Weight' min="0" max="255" required className='inputCreate' />
-                    </div>
-                        {errors.weight && (
-                            <p className='error-input'>{errors.weight}</p>
-                        )}
-
-                    <div className="dropdown">
-                        <select name='type1' onChange={handleInputChange} className='capitalizeText'>
-                            <option value="Type One">Type One</option>
-                            {pokeTypes.map(type =>
-                                <option value={type} key={type} className='capitalizeText'>{type}</option>
+                    
+                    <div className='left'>
+                        <div className='divInputCreate'>
+                            <input
+                                type='text' name='name' onChange={handleInputChange} placeholder="Name" value={input.name} required autoComplete='off' className='inputCreate' />
+                        </div>
+                            {errors.name && (
+                                <p className='error-input'>{errors.name}</p>
                             )}
-                        </select>
-                    </div>
-                    <div className="dropdown">
-                        <select name='type2' onChange={handleInputChange} className='capitalizeText'>
-                            <option value="Type Two">Type Two</option>
-                            {pokeTypes.map(type =>
-                                <option value={type} key={type} className='capitalizeText'>{type}</option>
+
+                        <div className='divInputCreate'>
+                            <input
+                                type='number' name='life' onChange={handleInputChange} value={input.life} placeholder='Life' min="0" max="255" required className='inputCreate' />
+                        </div>
+                            {errors.life && (
+                                <p className='error-input'>{errors.life}</p>
                             )}
-                        </select>
+
+                        <div className='divInputCreate'>
+                            <input
+                                type='number' name='attack' onChange={handleInputChange} value={input.attack} placeholder='Attack' min="0" max="255" required className='inputCreate' />
+                        </div>
+                            {errors.attack && (
+                                <p className='error-input'>{errors.attack}</p>
+                            )}
+
+                        <div className='divInputCreate'>
+                            <input
+                                type='number' name='defense' onChange={handleInputChange} value={input.defense} placeholder='Defense' min="0" max="255" required className='inputCreate' />
+                        </div>
+                            {errors.defense && (
+                                <p className='error-input'>{errors.defense}</p>
+                            )}
+                        <div className='divInputCreate'>
+                            <input
+                                type='number' name='speed' onChange={handleInputChange} value={input.speed} placeholder='Speed' min="0" max="255" required className='inputCreate' />
+                        </div>
+                        {errors.speed && (
+                            <p className='error-input'>{errors.speed}</p>
+                        )}
                     </div>
 
-                    {/* Photo */}
-                    <div>
-                        <input type="text" placeholder="Image" name="sprite" onChange={handleInputChange} value={input.sprite}/>
+                    <div className='rigth'>
+                    
+                        <div className='divInputCreate'>
+                            <input
+                                type='number' name='height' onChange={handleInputChange} value={input.height} placeholder='Height' min="0" max="255" required className='inputCreate' />
+                        </div>
+                        {errors.height && (
+                            <p className='error-input'>{errors.height}</p>
+                        )}
+
+                        <div className='divInputCreate'>
+                            <input
+                                type='number' name='weight' onChange={handleInputChange} value={input.weight} placeholder='Weight' min="0" max="255" required className='inputCreate' />
+                        </div>
+                            {errors.weight && (
+                                <p className='error-input'>{errors.weight}</p>
+                            )}
+
+                        <div className="dropdown" >
+                            <select name='type1' onChange={handleInputChange} className='capitalizeText'>
+                                <option value="Type One">Type One</option>
+                                {pokeTypes.map(type =>
+                                    <option value={type} key={type} className='capitalizeText'>{type}</option>
+                                )}
+                            </select>
+                        </div>
+                        <div className="dropdown">
+                            <select name='type2' onChange={handleInputChange} className='capitalizeText'>
+                                <option value="Type Two">Type Two</option>
+                                {pokeTypes.map(type =>
+                                    <option value={type} key={type} className='capitalizeText'>{type}</option>
+                                )}
+                            </select>
+                        </div>
+
+                        {/* Photo */}
+                        <div className='divInputCreate'>
+                            <input className='inputCreate' type="text" placeholder="Image" name="sprite" onChange={handleInputChange} value={input.sprite}/>
+                        </div>
                     </div>
 
                     <div>
