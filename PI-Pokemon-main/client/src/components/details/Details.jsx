062b10216a36c11f7a22fd32285d18cdfdc93639
jonasojeda <jonojed98@gmail.com>
2022-11-08 01:03:29 -0300

    Correction PokemonDetail component

--- PI-Pokemon-main/client/src/components/details/Details.jsx
+++ PI-Pokemon-main/client/src/components/details/Details.jsx
@@ -1,64 +1,11 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router';
-import { getPokemonDetail } from '../../actions/actions';
+import React from 'react'
 import imgTypes from '../../assets/helpers/TypesIcon';
-import Loader from '../../pages/loading/Loading.jsx';
-import LevelBar from '../levelBar/LevelBar';
 import './detail.css'
 
-export default function Details() {
-    const dispatch = useDispatch();
-    const pokemonDetail = useSelector(state => state.pokemonDetail);
-    const { id } = useParams();
-    useEffect(() => {
-      dispatch(getPokemonDetail(id.slice(1, id.length)))
-  }, [dispatch])
-
-  const { sprite, life, types, name, height, attack, defense, speed, weight } = pokemonDetail;
-
-    const truncateString = (str, num) => {
-        if (str.length <= num) return str
-        return str.slice(0, num) + '...'
-    }
+export default function Details({ sprite, life, types, name, height, attack, defense, speed, weight }) {
   return (
     <div className='detail-container'>
-
-      <div className='card-detail'>
-
-        <div className='name'>#{id} {name}</div>
-
-        <div className='left-container'>
-          <img src={sprite} alt="" />
-        </div>
-
-        <div className='rigth-container'>
-          <div className='general-info'>
-            <div className='general-box'>
-              <p>Height: {Number((height * 0.10).toFixed(1)) } m </p>
-            </div>
-
-            <div className='general-box'>
-              <p>Weight: {Number((weight * 0.10).toFixed(1)) } kg</p> 
-            </div>
-            
-            
-          </div>
-            <div className='specific-box'>
-              <p>Life: {life}</p>
-              <p>Defense: {defense}</p>
-              <p>Attack: {attack}</p>
-              <p>Speed: {speed}</p>
-            </div>
-          
-
-
-          
-        </div>
-        
-          
-      </div>
-      
+      {name}
     </div>
   )
 }
--- PI-Pokemon-main/client/src/pages/pokemonDetail/PokemonDetail.jsx
+++ PI-Pokemon-main/client/src/pages/pokemonDetail/PokemonDetail.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import { getPokemonDetail } from '../../actions/actions';
 import Details from '../../components/details/Details'
 import NavBar from '../../components/navBar/NavBar'
 import { Link } from 'react-router-dom'
 import './pokemonDetail.css'
+import Loading from '../loading/Loading'
 
 export default function PokemonDetail() {
+    const dispatch = useDispatch();
+    const pokemonDetail = useSelector(state => state.pokemonDetail);
+    const { id } = useParams();
+    useEffect(() => {
+    dispatch(getPokemonDetail(id.slice(1, id.length)))
+    }, [dispatch])
+
+const { sprite, life, types, name, height, attack, defense, speed, weight } = pokemonDetail;
+
+    const truncateString = (str, num) => {
+        if (str.length <= num) return str
+        return str.slice(0, num) + '...'
+    }
     
     return (
     <div className='container-pokemon-detail'>
@@ -15,7 +32,10 @@ export default function PokemonDetail() {
             <a>Return to home</a>
             </Link>
         </div>
-        <Details></Details>
+
+        {(sprite && life && types && name && height && attack && defense && speed && weight)? <Details name={name} sprite={sprite} life={life} types={types} height={height} attack={attack} defense={defense} speed={speed} weight={weight}></Details> : <Loading/> }
+
+        
     </div>
     
     )
